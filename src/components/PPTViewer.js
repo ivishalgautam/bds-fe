83f98b39c65d11ca9d53a01b5d7038e2852c8a7b
Vishal Gautam <ivishalgautamm@gmail.com>
2023-12-26 13:13:53 +0530

    fix document error

--- src/components/PPTViewer.js
+++ src/components/PPTViewer.js
@@ -1,30 +1,16 @@
-import { useEffect, useRef } from "react";
-
 export default function PdfViewerComponent({ embedUrl }) {
-  // const iframeRef = useRef(null);
-  // useEffect(() => {
-  //   const iframe = iframeRef.current;
-  //   const iframeDocument =
-  //     iframe.contentDocument || iframe.contentWindow.document;
-  //   const element = iframeDocument.querySelector(".ndfHFb-c4YZDc-Wrql6b");
-  //   console.log(iframe);
-  //   // element.style.display = "none";
-  // }, []);
-  //   let url = `https://docs.google.com/gview?url=${process.env.NEXT_PUBLIC_IMAGE_DOMAIN}/${embedUrl}&embedded=true`;
   let url = `https://docs.google.com/gview?url=${"https://magnitecorp.com/ppt/slides.pptx"}&embedded=true`;
 
   return (
     <div>
       <div style={{ width: "100%", height: "600px" }}>
         <iframe
-          // ref={iframeRef}
           src={url}
           width="100%"
           height="100%"
           frameBorder="0"
           allowFullScreen
           className="ppt-iframe"
-          sandbox="allow-same-origin allow-scripts allow-popups"
         ></iframe>
       </div>
     </div>
